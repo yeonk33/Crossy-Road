de0e079ab1e2185e87a7e74a81062163c1828608
LeeYeonkyung <55737485+yeonk33@users.noreply.github.com>
2025-03-26 18:11:27 +0900

    Player Input system 수정

--- Crossy-Road/Assets/Scripts/PlayerController.cs
+++ Crossy-Road/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,20 +7,73 @@ public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private float distance = 3.0f; // 한 칸 이동 거리
 	[SerializeField] private float speed = 5f;
-	private bool isMoving = false;
+	[SerializeField] private bool isMoving = false;
 	private PlayerInput playerInput;
-	private InputAction action;
+	private InputAction forwardAction;
+	private InputAction swipeAction;
+	private InputAction clickAction;
+	private Vector2 swipeStart;
+	private Vector2 swipeEnd;
+	[SerializeField] private bool isDragging;
+	[SerializeField] private float minDis = 50f;
 
 
 	private void Awake()
 	{
 		playerInput = GetComponent<PlayerInput>();
 
-		action = playerInput.actions["Forward"];
-		action.performed += Action_performed;
+		// 클릭 시 전진
+		forwardAction = playerInput.actions["Forward"];
+		forwardAction.performed += ForwardAction_performed;
+
+		// 클릭한채로 스와이프 시 좌/우/뒤로 이동
+		swipeAction = playerInput.actions["Swipe"];
+		swipeAction.performed += SwipeAction_performed;
+		swipeAction.canceled += SwipeAction_canceled;
+
+		clickAction = playerInput.actions["Click"];
+		clickAction.performed += ClickAction_performed;
+		clickAction.canceled += ClickAction_canceled;
 	}
 
-	private void Action_performed(InputAction.CallbackContext obj)
+	private void ClickAction_canceled(InputAction.CallbackContext obj)
+	{
+		if (!isDragging) return;
+
+		isDragging = false;
+		swipeEnd = swipeAction.ReadValue<Vector2>();
+		Vector2 delta = swipeEnd - swipeStart;
+		Debug.Log($"{swipeEnd} - {swipeStart} = {delta}");
+		if (delta.magnitude < minDis) return;   // 너무 조금 스와이프한건 무시
+
+		Vector3 dir = Vector3.zero;
+		if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y)) {  // 좌우로 더 많이 움직임
+			dir = delta.x > 0 ? Vector3.right : Vector3.left;
+		} else {
+			dir = delta.y > 0 ? Vector3.back : Vector3.forward;
+		}
+
+		if (!isMoving) StartCoroutine(Move(transform.position + dir * distance));
+	}
+
+	private void ClickAction_performed(InputAction.CallbackContext obj)
+	{
+		swipeStart = swipeAction.ReadValue<Vector2>();
+		isDragging = true;
+	}
+
+	private void SwipeAction_canceled(InputAction.CallbackContext obj)
+	{
+		//swipeEnd = Mouse.current.position.ReadValue();
+
+	}
+
+	private void SwipeAction_performed(InputAction.CallbackContext obj)
+	{
+		//swipeStart = Mouse.current.position.ReadValue();
+	}
+
+	private void ForwardAction_performed(InputAction.CallbackContext obj)
 	{
 		OnMove();
 	}
@@ -46,8 +100,6 @@ public class PlayerController : MonoBehaviour
 			// 이동
 			Vector3 newPos = Vector3.MoveTowards(startPos, dest, timer / duration * Vector3.Distance(startPos, dest));
 			newPos.y += Mathf.Sin(t * Mathf.PI) * height; // y축 점프
-
-			// 최종 위치 적용
 			transform.position = newPos;
 
 			yield return null;
